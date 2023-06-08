2701abc58acffbf9a05c738d9fc1d4c0ca1dc2ca
leechifang <fcl411991@gmail.com>
2023-06-08 16:16:03 +0800

    sumArray chenge array method map -> reduce

--- 09/sumArray.js
+++ 09/sumArray.js
@@ -1,12 +1,18 @@
 export function sumArray(firstArray, secondArray) {
   if (firstArray.length === secondArray.length) {
-    return firstArray.map((value, index) => value + secondArray[index]);
+    return firstArray.reduce((accumulator, currentValue, currentIndex) => {
+      accumulator.push(currentValue + secondArray[currentIndex]);
+      return accumulator;
+    }, []);
   }
   const shortArray = firstArray.length < secondArray.length;
   const longArray = shortArray ? secondArray : firstArray;
   const fillArray = shortArray ? firstArray : secondArray;
   const filledArray = fillShortArray(fillArray, longArray.length);
-  return longArray.map((value, index) => value + filledArray[index]);
+  return longArray.reduce((accumulator, currentValue, currentIndex) => {
+    accumulator.push(currentValue + filledArray[currentIndex]);
+    return accumulator;
+  }, []);
 }
 
 function fillShortArray(originArray, arrayLength) {
