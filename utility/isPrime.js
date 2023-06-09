e1e78bdc113fa5d376d2cf942b5cb68761453e3b
leechifang <fcl411991@gmail.com>
2023-06-09 18:31:53 +0800

    Q10 modify: comment

--- utility/isPrime.js
+++ utility/isPrime.js
@@ -1,3 +1,7 @@
+// 一個自然數若恰有兩個正因數（1及此數本身），則稱之為質數。
+// 找質數反證法：找到該值的開根號並取於數等於0就不是質數。
+//             0,1不是質數，2是所有質數中唯一偶數。
+
 export function isPrime(number) {
   if (number === 0 || number === 1) {
     return false;
